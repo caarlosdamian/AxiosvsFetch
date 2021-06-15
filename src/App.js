026e5b50e47a3a7d808fb34fa2c2ce082b430ec9
caarlosdamian <dapecarlos@gmail.com>
2021-06-15 11:14:52 -0500

    added Axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
+import Axios from "axios";
 import { useEffect, useState } from "react";
 
 const App = (_) => {
-  const [name, setname] = useState(null);
-  
+  const [name, setName] = useState(null);
+  const [nameAxios, setNameAxios] = useState([]);
 
   useEffect(() => {
+    Axios.get("https://pokeapi.co/api/v2/pokemon/ditto").then((res) => {
+      setNameAxios(res.data.name);
+    });
+
     fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-      .then((res) => res.json().then(console.log(res)))
-      .then((res) => setname(res.name));
+      .then((res) => res.json())
+      .then((res) => setName(res.name));
   }, []);
-  return <p>{name}</p>;
+  return (
+    <div className='forms'>
+      <h1>Formas GET para una API</h1>
+      <p>Fetch: {name}</p>
+      <p>Axios: {nameAxios}</p>
+    </div>
+  );
 };
 export default App;
